75ccacb06c4979ba491723f3b2b36fd4002ecaed
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-04-12 17:02:40 -0600

    Finished permission:set command

--- tools/v2-cli/src/commands/permission/set.ts
+++ tools/v2-cli/src/commands/permission/set.ts
@@ -1,6 +1,9 @@
 import { flags } from "@oclif/command";
 import { PublicKey } from "@solana/web3.js";
-import { PermissionAccount } from "@switchboard-xyz/switchboard-v2";
+import {
+  PermissionAccount,
+  SwitchboardPermission,
+} from "@switchboard-xyz/switchboard-v2";
 import { loadSwitchboardAccount } from "@switchboard-xyz/v2-utils-ts";
 import * as chalk from "chalk";
 import BaseCommand from "../../BaseCommand";
@@ -39,17 +42,53 @@ export default class PermissionSet extends BaseCommand {
     });
     const permission = await permissionAccount.loadData();
 
-    const grantee = await loadSwitchboardAccount(
+    // check and load granters account type
+    const [granteeAccountType, grantee] = await loadSwitchboardAccount(
       this.program,
       permission.grantee
     );
-
-    // check and load granters account type
+    let authorityKey: PublicKey;
+    switch (granteeAccountType) {
+      case "OracleQueueAccountData":
+        const data = await grantee.loadData();
+        authorityKey = data.authority;
+        break;
+      default:
+        throw new Error("Grantee should be a OracleQueueAccount");
+    }
+    const authority = await this.loadAuthority(flags.authority, authorityKey);
 
     // check and load grantees account type, and assign permissions based on type
+    let assignedPermission: SwitchboardPermission;
+    const [granterAccountType, granter] = await loadSwitchboardAccount(
+      this.program,
+      permission.granter
+    );
+    switch (granterAccountType) {
+      case "OracleAccountData":
+        assignedPermission = SwitchboardPermission.PERMIT_ORACLE_HEARTBEAT;
+        break;
+      case "AggregatorAccountData":
+        assignedPermission = SwitchboardPermission.PERMIT_ORACLE_QUEUE_USAGE;
+        break;
+      case "VrfAccountData":
+        assignedPermission = SwitchboardPermission.PERMIT_VRF_REQUESTS;
+        break;
+      default:
+        throw new Error(
+          "Granter must be an AggregatorAccount, OracleAccount, or VrfAccount"
+        );
+    }
+
+    // set the permission
+    const txn = await permissionAccount.set({
+      authority,
+      enable: !flags.disable,
+      permission: assignedPermission,
+    });
 
     if (this.silent) {
-      console.log(permission.publicKey.toString());
+      console.log(txn);
     } else {
       this.logger.log(
         `${chalk.green(`${CHECK_ICON}Permissions set successfully`)}`
