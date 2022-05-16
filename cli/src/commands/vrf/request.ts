b5aef2a2c5f6dda8b67e0f0ab685d0054d49f48c
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-05-16 13:24:35 -0600

    fixed vrf:request command

--- cli/src/commands/vrf/request.ts
+++ cli/src/commands/vrf/request.ts
@@ -1,7 +1,10 @@
 import { flags } from "@oclif/command";
-import { PublicKey } from "@solana/web3.js";
+import * as spl from "@solana/spl-token";
+import { PublicKey, SYSVAR_RECENT_BLOCKHASHES_PUBKEY } from "@solana/web3.js";
 import {
   OracleQueueAccount,
+  PermissionAccount,
+  ProgramStateAccount,
   programWallet,
   VrfAccount,
 } from "@switchboard-xyz/switchboard-v2";
@@ -46,6 +49,15 @@ export default class VrfRequest extends BaseCommand {
     });
     const queue = await queueAccount.loadData();
     const tokenMint = await queueAccount.loadMint();
+    const [programStateAccount, stateBump] = ProgramStateAccount.fromSeed(
+      this.program
+    );
+    const [permissionAccount, permissionBump] = PermissionAccount.fromSeed(
+      this.program,
+      queue.authority,
+      queueAccount.publicKey,
+      vrfAccount.publicKey
+    );
 
     const authority = await this.loadAuthority(flags.authority, vrf.authority);
     const funderAuthority = flags.funderAuthority
@@ -58,11 +70,33 @@ export default class VrfRequest extends BaseCommand {
       )
     ).address;
 
-    const signature = await vrfAccount.requestRandomness({
-      authority,
-      payerAuthority: funderAuthority,
-      payer: funderTokenWallet,
-    });
+    // const signature = await vrfAccount.requestRandomness({
+    //   authority,
+    //   payerAuthority: funderAuthority,
+    //   payer: funderTokenWallet,
+    // });
+
+    const signature = await this.program.methods
+      .vrfRequestRandomness({
+        stateBump,
+        permissionBump,
+      })
+      .accounts({
+        authority: authority.publicKey,
+        vrf: vrfAccount.publicKey,
+        oracleQueue: queueAccount.publicKey,
+        queueAuthority: queue.authority,
+        dataBuffer: queue.dataBuffer,
+        permission: permissionAccount.publicKey,
+        escrow: vrf.escrow,
+        payerWallet: funderTokenWallet,
+        payerAuthority: funderAuthority.publicKey,
+        recentBlockhashes: SYSVAR_RECENT_BLOCKHASHES_PUBKEY,
+        programState: programStateAccount.publicKey,
+        tokenProgram: spl.TOKEN_PROGRAM_ID,
+      })
+      .signers([authority, funderAuthority])
+      .rpc();
 
     if (this.silent) {
       console.log(signature);
