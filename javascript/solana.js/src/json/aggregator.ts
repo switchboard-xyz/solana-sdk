55a02f0672f0bab9f540eb00d9573b9bde7a9c18
Conner Gallagher <ConnerNGallagher@gmail.com>
2022-12-20 08:37:43 -0700

    solana.js: add historyLimit to aggregator JSON

--- javascript/solana.js/src/json/aggregator.ts
+++ javascript/solana.js/src/json/aggregator.ts
@@ -21,6 +21,8 @@ export class AggregatorJson implements CreateQueueFeedParams {
   expiration: number;
   varianceThreshold: number;
   forceReportPeriod: number;
+  historyLimit?: number;
+
   disableCrank: boolean;
   crankIndex?: number;
 
@@ -65,9 +67,15 @@ export class AggregatorJson implements CreateQueueFeedParams {
     this.expiration = parseNumber(object, 'expiration', 0);
     this.varianceThreshold = parseNumber(object, 'varianceThreshold', 0);
     this.forceReportPeriod = parseNumber(object, 'forceReportPeriod', 0);
+    this.historyLimit =
+      'historyLimit' in object
+        ? Number.parseInt(object.historyLimit)
+        : undefined;
+
     this.disableCrank = parseBoolean(object, 'disableCrank', false);
     this.crankIndex =
       'crankIndex' in object ? Number(object.crankIndex) : undefined;
+
     this.basePriorityFee =
       'basePriorityFee' in object
         ? Number.parseInt(object.basePriorityFee)
@@ -125,7 +133,12 @@ export class AggregatorJson implements CreateQueueFeedParams {
       expiration: this.expiration,
       varianceThreshold: this.varianceThreshold,
       forceReportPeriod: this.forceReportPeriod,
+      historyLimit: this.historyLimit,
       disableCrank: this.disableCrank,
+      basePriorityFee: this.basePriorityFee,
+      priorityFeeBump: this.priorityFeeBump,
+      priorityFeeBumpPeriod: this.priorityFeeBumpPeriod,
+      maxPriorityFeeMultiplier: this.maxPriorityFeeMultiplier,
       fundAmount: this.fundAmount,
       keypair: keypairToString(this.keypair),
       authority: this.authority ? keypairToString(this.authority) : undefined,
