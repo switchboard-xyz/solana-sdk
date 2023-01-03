bebda80fbd04344f76e9f7d3df7e3f474fafd229
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-01-03 14:12:54 -0700

    solana.js: published 2.0.66
    - cleaned up SwitchboardEvents

--- javascript/solana.js/src/SwitchboardEvents.ts
+++ javascript/solana.js/src/SwitchboardEvents.ts
@@ -6,17 +6,41 @@ export type AggregatorAddJobEvent = {
   jobPubkey: anchor.web3.PublicKey;
 };
 
-export type AggregatorRemoveJobEvent = {
+export type AggregatorCrankEvictionEvent = {
+  crankPubkey: anchor.web3.PublicKey;
+  aggregatorPubkey: anchor.web3.PublicKey;
+  reason?: number;
+  timestamp: anchor.BN;
+};
+
+export type AggregatorInitEvent = {
   feedPubkey: anchor.web3.PublicKey;
-  jobPubkey: anchor.web3.PublicKey;
 };
 
 export type AggregatorLockEvent = {
   feedPubkey: anchor.web3.PublicKey;
 };
 
-export type AggregatorInitEvent = {
+export type AggregatorOpenRoundEvent = {
   feedPubkey: anchor.web3.PublicKey;
+  oraclePubkeys: anchor.web3.PublicKey[];
+  jobPubkeys: anchor.web3.PublicKey[];
+  remainingFunds: anchor.BN;
+  queueAuthority: anchor.web3.PublicKey;
+};
+
+export type AggregatorRemoveJobEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  jobPubkey: anchor.web3.PublicKey;
+};
+
+export type AggregatorSaveResultEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  value: types.BorshDecimal;
+  slot: anchor.BN;
+  timestamp: anchor.BN;
+  oraclePubkey: anchor.web3.PublicKey;
+  jobValues: Array<types.BorshDecimal>;
 };
 
 export type AggregatorSetAuthorityEvent = {
@@ -29,51 +53,6 @@ export type AggregatorSetConfigsEvent = {
   feedPubkey: anchor.web3.PublicKey;
 };
 
-export type PermissionSetEvent = {
-  permissionKey: anchor.web3.PublicKey;
-  permission: types.SwitchboardPermissionKind;
-  enable: boolean;
-};
-
-export type VrfRequestRandomnessEvent = {
-  vrfPubkey: anchor.web3.PublicKey;
-  oraclePubkeys: anchor.web3.PublicKey[];
-  loadAmount: anchor.BN;
-  existingAmount: anchor.BN;
-};
-
-export type VrfRequestEvent = {
-  vrfPubkey: anchor.web3.PublicKey;
-  oraclePubkeys: anchor.web3.PublicKey[];
-};
-
-export type VrfProveEvent = {
-  vrfPubkey: anchor.web3.PublicKey;
-  oraclePubkey: anchor.web3.PublicKey;
-  authorityPubkey: anchor.web3.PublicKey;
-};
-
-export type VrfVerifyEvent = {
-  vrfPubkey: anchor.web3.PublicKey;
-  oraclePubkey: anchor.web3.PublicKey;
-  authorityPubkey: anchor.web3.PublicKey;
-  amount: anchor.BN;
-};
-
-export type VrfCallbackPerformedEvent = {
-  vrfPubkey: anchor.web3.PublicKey;
-  oraclePubkey: anchor.web3.PublicKey;
-  amount: anchor.BN;
-};
-
-export type AggregatorOpenRoundEvent = {
-  feedPubkey: anchor.web3.PublicKey;
-  oraclePubkeys: anchor.web3.PublicKey[];
-  jobPubkeys: anchor.web3.PublicKey[];
-  remainingFunds: anchor.BN;
-  queueAuthority: anchor.web3.PublicKey;
-};
-
 export type AggregatorValueUpdateEvent = {
   feedPubkey: anchor.web3.PublicKey;
   value: types.SwitchboardDecimalFields;
@@ -83,7 +62,64 @@ export type AggregatorValueUpdateEvent = {
   oracleValues: types.SwitchboardDecimalFields[];
 };
 
+export type BufferRelayerOpenRoundEvent = {
+  relayerPubkey: anchor.web3.PublicKey;
+  jobPubkey: anchor.web3.PublicKey;
+  oraclePubkeys: anchor.web3.PublicKey[];
+  remainingFunds: anchor.BN;
+  queue: anchor.web3.PublicKey;
+};
+
+export type CrankLeaseInsufficientFundsEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  leasePubkey: anchor.web3.PublicKey;
+};
+
+export type CrankPopExpectedFailureEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  leasePubkey: anchor.web3.PublicKey;
+};
+
+export type FeedPermissionRevokedEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  timestamp: anchor.BN;
+};
+
+export type GarbageCollectFailureEvent = {
+  queuePubkey: anchor.web3.PublicKey;
+};
+
+export type LeaseFundEvent = {
+  leasePubkey: anchor.web3.PublicKey;
+  funder: anchor.web3.PublicKey;
+  amount: anchor.BN;
+  timestamp: anchor.BN;
+};
+
+export type LeaseWithdrawEvent = {
+  leasePubkey: anchor.web3.PublicKey;
+  walletPubkey: anchor.web3.PublicKey;
+  previousAmount: anchor.BN;
+  newAmount: anchor.BN;
+  timestamp: anchor.BN;
+};
+
+export type OracleBootedEvent = {
+  queuePubkey: anchor.web3.PublicKey;
+  oraclePubkey: anchor.web3.PublicKey;
+};
+
 export type OracleRewardEvent = {
+  feedPubkey: anchor.web3.PublicKey;
+  leasePubkey: anchor.web3.PublicKey;
+  oraclePubkey: anchor.web3.PublicKey;
+  walletPubkey: anchor.web3.PublicKey;
+  amount: anchor.BN;
+  roundSlot: anchor.BN;
+  timestamp: anchor.BN;
+};
+
+export type OracleSlashEvent = {
   feedPubkey: anchor.web3.PublicKey;
   leasePubkey: anchor.web3.PublicKey;
   oraclePubkey: anchor.web3.PublicKey;
@@ -102,29 +138,17 @@ export type OracleWithdrawEvent = {
   timestamp: anchor.BN;
 };
 
-export type LeaseWithdrawEvent = {
-  leasePubkey: anchor.web3.PublicKey;
-  walletPubkey: anchor.web3.PublicKey;
-  previousAmount: anchor.BN;
-  newAmount: anchor.BN;
-  timestamp: anchor.BN;
+export type PermissionSetEvent = {
+  permissionKey: anchor.web3.PublicKey;
+  permission: types.SwitchboardPermissionKind;
+  enable: boolean;
 };
 
-export type OracleSlashEvent = {
+export type PriorityFeeReimburseEvent = {
   feedPubkey: anchor.web3.PublicKey;
-  leasePubkey: anchor.web3.PublicKey;
-  oraclePubkey: anchor.web3.PublicKey;
-  walletPubkey: anchor.web3.PublicKey;
-  amount: anchor.BN;
-  roundSlot: anchor.BN;
+  slot: anchor.BN;
   timestamp: anchor.BN;
-};
-
-export type LeaseFundEvent = {
-  leasePubkey: anchor.web3.PublicKey;
-  funder: anchor.web3.PublicKey;
-  amount: anchor.BN;
-  timestamp: anchor.BN;
+  fee: anchor.BN;
 };
 
 export type ProbationBrokenEvent = {
@@ -133,64 +157,77 @@ export type ProbationBrokenEvent = {
   timestamp: anchor.BN;
 };
 
-export type FeedPermissionRevokedEvent = {
-  feedPubkey: anchor.web3.PublicKey;
-  timestamp: anchor.BN;
-};
-
-export type GarbageCollectFailureEvent = {
-  queuePubkey: anchor.web3.PublicKey;
-};
-
-export type OracleBootedEvent = {
-  queuePubkey: anchor.web3.PublicKey;
+export type VrfCallbackPerformedEvent = {
+  vrfPubkey: anchor.web3.PublicKey;
   oraclePubkey: anchor.web3.PublicKey;
+  amount: anchor.BN;
 };
 
-export type CrankLeaseInsufficientFundsEvent = {
-  feedPubkey: anchor.web3.PublicKey;
-  leasePubkey: anchor.web3.PublicKey;
+export type VrfProveEvent = {
+  vrfPubkey: anchor.web3.PublicKey;
+  oraclePubkey: anchor.web3.PublicKey;
+  authorityPubkey: anchor.web3.PublicKey;
 };
 
-export type CrankPopExpectedFailureEvent = {
-  feedPubkey: anchor.web3.PublicKey;
-  leasePubkey: anchor.web3.PublicKey;
-};
-
-export type BufferRelayerOpenRoundEvent = {
-  relayerPubkey: anchor.web3.PublicKey;
-  jobPubkey: anchor.web3.PublicKey;
+export type VrfRequestEvent = {
+  vrfPubkey: anchor.web3.PublicKey;
   oraclePubkeys: anchor.web3.PublicKey[];
-  remainingFunds: anchor.BN;
-  queue: anchor.web3.PublicKey;
+};
+
+export type VrfRequestRandomnessEvent = {
+  vrfPubkey: anchor.web3.PublicKey;
+  oraclePubkeys: anchor.web3.PublicKey[];
+  loadAmount: anchor.BN;
+  existingAmount: anchor.BN;
+  alpha: Buffer;
+  counter: anchor.BN;
+};
+
+export type VrfVerifyEvent = {
+  vrfPubkey: anchor.web3.PublicKey;
+  oraclePubkey: anchor.web3.PublicKey;
+  authorityPubkey: anchor.web3.PublicKey;
+  amount: anchor.BN;
 };
 
 export type SwitchboardEvents = {
   AggregatorAddJobEvent: AggregatorAddJobEvent;
-  AggregatorRemoveJobEvent: AggregatorRemoveJobEvent;
-  AggregatorLockEvent: AggregatorLockEvent;
+  AggregatorCrankEvictionEvent: AggregatorCrankEvictionEvent;
   AggregatorInitEvent: AggregatorInitEvent;
+  AggregatorLockEvent: AggregatorLockEvent;
+  AggregatorOpenRoundEvent: AggregatorOpenRoundEvent;
+  AggregatorRemoveJobEvent: AggregatorRemoveJobEvent;
+  AggregatorSaveResultEvent: AggregatorSaveResultEvent;
   AggregatorSetAuthorityEvent: AggregatorSetAuthorityEvent;
   AggregatorSetConfigsEvent: AggregatorSetConfigsEvent;
-  PermissionSetEvent: PermissionSetEvent;
-  VrfRequestRandomnessEvent: VrfRequestRandomnessEvent;
-  VrfRequestEvent: VrfRequestEvent;
-  VrfProveEvent: VrfProveEvent;
-  VrfVerifyEvent: VrfVerifyEvent;
-  VrfCallbackPerformedEvent: VrfCallbackPerformedEvent;
-  AggregatorOpenRoundEvent: AggregatorOpenRoundEvent;
   AggregatorValueUpdateEvent: AggregatorValueUpdateEvent;
-  OracleRewardEvent: OracleRewardEvent;
-  OracleWithdrawEvent: OracleWithdrawEvent;
-  LeaseWithdrawEvent: LeaseWithdrawEvent;
-  OracleSlashEvent: OracleSlashEvent;
-  LeaseFundEvent: LeaseFundEvent;
-  ProbationBrokenEvent: ProbationBrokenEvent;
-  FeedPermissionRevokedEvent: FeedPermissionRevokedEvent;
-  GarbageCollectFailureEvent: GarbageCollectFailureEvent;
-  OracleBootedEvent: OracleBootedEvent;
-  AggregatorCrankEvictionEvent: {};
+
+  BufferRelayerOpenRoundEvent: BufferRelayerOpenRoundEvent;
+
   CrankLeaseInsufficientFundsEvent: CrankLeaseInsufficientFundsEvent;
   CrankPopExpectedFailureEvent: CrankPopExpectedFailureEvent;
-  BufferRelayerOpenRoundEvent: BufferRelayerOpenRoundEvent;
+
+  FeedPermissionRevokedEvent: FeedPermissionRevokedEvent;
+
+  GarbageCollectFailureEvent: GarbageCollectFailureEvent;
+
+  LeaseFundEvent: LeaseFundEvent;
+  LeaseWithdrawEvent: LeaseWithdrawEvent;
+
+  OracleBootedEvent: OracleBootedEvent;
+  OracleRewardEvent: OracleRewardEvent;
+  OracleSlashEvent: OracleSlashEvent;
+  OracleWithdrawEvent: OracleWithdrawEvent;
+
+  PermissionSetEvent: PermissionSetEvent;
+
+  PriorityFeeReimburseEvent: PriorityFeeReimburseEvent;
+
+  ProbationBrokenEvent: ProbationBrokenEvent;
+
+  VrfCallbackPerformedEvent: VrfCallbackPerformedEvent;
+  VrfProveEvent: VrfProveEvent;
+  VrfRequestEvent: VrfRequestEvent;
+  VrfRequestRandomnessEvent: VrfRequestRandomnessEvent;
+  VrfVerifyEvent: VrfVerifyEvent;
 };
