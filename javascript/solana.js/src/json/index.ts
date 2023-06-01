131888bd889c03942329b8736b557bf54e65e999
Conner Gallagher <ConnerNGallagher@gmail.com>
2023-06-01 11:04:37 -0600

    rm json dir

--- javascript/solana.js/src/json/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './aggregator';
-export * from './crank';
-export * from './job';
-export * from './network';
-export * from './oracle';
-export * from './queue';
-export * from './vrf';
